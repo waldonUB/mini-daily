c464bfa0d7c6c410f50b0a5dba9771b77c5517d2
waldonUB <852727515@qq.com>
2019-10-08 08:44:30 +0800

    init project

---
+++ src/components/GcInput.vue
@@ -0,0 +1,53 @@
+<template>
+	<div class="input-box">
+        <!-- <input type="text" class="icon-input"> -->
+        <svg class="icon" aria-hidden="true" :class="locate">
+            <use xlink:href="#icon-icon-11"></use>
+        </svg>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'GcInput',
+    props: {
+        iconCode: {
+            default: ''
+        },
+        locate: {
+            default: 'prefix'
+        }
+    },
+    computed: {
+    }
+
+}
+</script>
+
+<style lang="less" scoped>
+.input-box {
+    position: relative;
+    .icon-input {
+        width: 100%;
+        height: 65rpx;
+        background-color: rgb(237, 241, 242);
+        outline: none;
+        border-radius: 8rpx;
+    }
+    .prefix {
+        position: absolute;
+        left: 8rpx;
+        top: 50%;
+        transform: translateY(-50%);
+        z-index: 9;
+    }
+    .suffix {
+        position: absolute;
+        right: 8rpx;
+        top: 50%;
+        transform: translateY(-50%);
+    }
+
+}
+
+</style>
